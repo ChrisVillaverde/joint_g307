f58b68b0b35b7c5aad5fe2c585dfb1c26a9174ce
Russell <russell.tchamba@gmail.com>
2022-09-22 00:16:18 +0200

    summary added

--- js/board.js
+++ js/board.js
@@ -10,11 +10,11 @@ let Alltasks_keys = [{
     'name': 'Design',
     'title': 'Social media strategy',
     'description':'Develop and campaign for brand positioning',
-    'category': 'todo'
+    'category': 'done'
 },
 {
     'id': 2,
-    'name': 'Backoffice',
+    'name': 'Marketing',
     'title': 'Social media strategy',
     'description':'Develop and campaign for brand positioning',
     'category': 'done'
@@ -28,13 +28,17 @@ let Alltasks_keys = [{
 },
 {
     'id': 4,
-    'name': 'Marketing',
-    'title': 'Social media strategy',
-    'description':'Develop and campaign for brand positioning',
+    'name': 'Backoffice',
+    'title': 'Accounting invoices',
+    'description':'Write open invoices for customer',
     'category': 'feedback'
 }];
 
 let currentDraggedElement ;
+let todosTasksNumber =0;
+let doneTasksNumber =0;
+let feedbackTasksNumber =0;
+let progressTasksNumber =0;
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -64,6 +68,7 @@ function allowDrop(ev) {
     updateProgressHTML();
     updateFeedbackHTML();
     updateDoneHTML();
+    /* dashboard(); */
   }
 
 
@@ -82,7 +87,7 @@ function allowDrop(ev) {
     document.getElementById('alltasks_progress').innerHTML = '';
     for (let index = 0; index < progresses.length; index++) {
         const element = progresses[index];
-        document.getElementById('alltasks_progress').innerHTML += generateTasksHTML(element);
+        document.getElementById('alltasks_progress').innerHTML += generateTasksStatusHTML(element);
         
     }
     
@@ -93,7 +98,7 @@ function updateFeedbackHTML(){
     document.getElementById('alltasks_feedback').innerHTML = '';
     for (let index = 0; index < feedbacks.length; index++) {
         const element = feedbacks[index];
-        document.getElementById('alltasks_feedback').innerHTML += generateTasksHTML(element);
+        document.getElementById('alltasks_feedback').innerHTML += generateTasksStatusHTML(element);
         
     }
     
@@ -118,7 +123,7 @@ function startDragging(id){
 function generateTasksHTML(element){
 
     return /*html*/ `
-    <div class="doneTask_1" class="allTasks_design" draggable="true" ondragstart="startDragging(${element['id']})">
+    <div class="doneTask_1" draggable="true" ondragstart="startDragging(${element['id']})">
                 <div id="doneTaskCard-child">
                     <div id="doneTaskCard">
                         <div id="doneTaskCard-name"><span id="doneTaskCard-text">${element['name']}</span></div>
@@ -147,6 +152,136 @@ function generateTasksHTML(element){
                 </div>
                 
             </div>
+
+
+    `;
+}
+
+function generateTasksStatusHTML(element){
+
+    return /*html*/ `
+    <div class="feedbackTask_1" draggable="true" ondragstart="startDragging(${element['id']})">
+        <div id="feedbackTaskCard">
+        <div id="feedbackTaskCard-name"><span id="feedbackTaskCard-text">${element['name']}</span></div>
+        <div class="taskCard-title">
+            <div class="taskCard-description">
+                <span class="taskCard-description-title">${element['title']}</span>
+                <span class="taskCard-description-text">${element['description']}</span>
+            </div>
+            <div id="user-priority">
+                <div id="user-id">
+                    <div class="user-id-child_1"> <span class="userName" >SM</span> </div>
+                    <div class="user-id-child_2"> <span class="userName" >MV</span> </div>
+
+                </div>
+                <div id="taskPriority"> <img id="taskPriority-img" src="./assets/img/medium.png" alt=""> </div>
+            </div>
+        </div>
+                
+    </div>
+
+
+    `;
+}
+
+function dashboard(){
+    document.getElementById('greet').innerHTML =`<b id="summary-greeting" > Good morning </b>`; ;
+    document.getElementById('dashboard').innerHTML =generateDashboardHTML() ;
+}
+
+function generateDashboardHTML(){
+    progressTasksNumber=Alltasks_keys.filter(t => t['category'] == 'progress');
+    todosTasksNumber=Alltasks_keys.filter(t => t['category'] == 'todo');
+    feedbackTasksNumber=Alltasks_keys.filter(t => t['category'] == 'feedback');
+    doneTasksNumber=Alltasks_keys.filter(t => t['category'] == 'done');
+
+    return /*html*/ `
+     <div id="dashboard-child1">
+            <div id="dashboard-child11">
+                <div id="urgent-dashboard">
+                   <div id="urgent-dashboard-child">
+                        <img id="urgent-img" src="./assets/img/urgent.png" alt="">
+                        <span id="urgentTasksNumber">1</span>
+                   </div> 
+                    
+                    <span id="urgent-text">Tasks Urgent</span>
+
+                </div>
+
+                <img src="./assets/img/strich_summary.png" alt="">
+
+                <div id="urgent-dashboard-child2">
+                    <span id="urgent-date">October 16, 2022</span>
+                    <span id="date-text">Upcoming Deadline</span> 
+
+                </div>
+
+            </div>
+            <div id="dashboard-child12">
+                <div id="todos-dashboard">
+                    <div id="todos-dashboard-child">
+                         <img id="todos-img" src="./assets/img/board.png" alt="">
+                         <span id="todosTasksNumber">${todosTasksNumber.length}</span>
+                    </div> 
+                     
+                     <span id="todos-text">Tasks To-do</span>
+ 
+                </div>
+
+            </div>
+
+        </div>
+
+        <div id="dashboard-child2">
+            <div id="dashboard-child21" >
+                <div class="dashboard-child2-all">
+                     <div id="board-dashboard-child" class="dashboard-child2x">
+                        <img id="board-img" src="./assets/img/board.png" alt="">
+                        <span id="boardTasksNumber" class="tasksNumber">${Alltasks_keys.length}</span>
+                    </div> 
+                
+                    <span id="board-text">Tasks in Board</span>
+                </div>
+               
+
+            </div>
+            <div id="dashboard-child22">
+                <div class="dashboard-child2-all">
+                    <div id="progress-dashboard-child" class="dashboard-child2x">
+                        <img id="progress-img" src="./assets/img/progress.png" alt="">
+                        <span id="progressTasksNumber" class="tasksNumber">${progressTasksNumber.length}</span>
+                    </div> 
+                    
+                    <span id="progress-text">Tasks in Progress</span>
+                </div>
+
+            </div>
+
+            <div id="dashboard-child23">
+                <div class="dashboard-child2-all">
+                    <div id="feedback-dashboard-child" class="dashboard-child2x">
+                        <img id="feedback-img" src="./assets/img/feedback.png" alt="">
+                        <span id="feedbackTasksNumber" class="tasksNumber">${feedbackTasksNumber.length}</span>
+                    </div> 
+                    
+                    <span id="feedback-text">Awaiting Feedback</span>
+                </div>
+
+            </div>
+            <div id="dashboard-child24">
+                <div class="dashboard-child2-all">
+                    <div id="done-dashboard-child" class="dashboard-child2x">
+                        
+                        <img id="done-img" src="./assets/img/done.png" alt="">
+                        <span id="doneTasksNumber" class="tasksNumber">${doneTasksNumber.length}</span>
+                    </div> 
+                    
+                    <span id="done-text">Tasks Done</span>
+                </div>
+
+            </div>
+
+        </div>
 
 
     `;
