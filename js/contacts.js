1817df5c041166e8179d994250bdea60f68bb22c
Manuel-Greulich <manuelgreulich@yahoo.com>
2022-09-25 21:37:46 +0200

    worked on contacts.js

--- js/contacts.js
+++ js/contacts.js
@@ -33,3 +33,36 @@ let contacts = [{
     'name': 'Tatjana Wolf',
     'Email': 'wolf@gmail.com'
 },];
+
+
+
+// Function shows Contact Book with Contacts //
+
+function showContacts() {
+  document.getElementById('contact-book').innerHTML = ``;
+
+  for (let i = 0; i < contacts.length; i++) {
+    // const element = array[i];
+    document.getElementById('contact-book').innerHTML += `
+
+
+        <div class="content-left"
+            <div class="contact-box">
+            
+                <div>
+                    <img class="contact-img" src="/assets/img/profile-dummy.png" alt="">
+                </div>
+                
+                <div class="contact-info">
+                    <span>${contacts[i]['name']}</span>
+                    <span>${contacts[i]['Email']}</span>
+                </div>         
+            </div> 
+        </div>    
+    `;
+    
+  }
+}
+
+
+
