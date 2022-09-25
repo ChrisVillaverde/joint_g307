89f382cb70536d525078d63f027e5dc3b148ad8d
Manuel-Greulich <manuelgreulich@yahoo.com>
2022-09-25 17:29:08 +0200

    add: contacts.js

---
+++ js/contacts.js
@@ -0,0 +1,35 @@
+let contacts = [{
+    'name': 'Anton Mayer',
+    'Email': 'anton@gmail.com'
+},
+{
+    'name': 'Anja Schulz',
+    'Email': 'schulz@hotmail.com'
+},
+{
+    'name': 'Benedikt Ziegler',
+    'Email': 'benedikt@gmail.com'
+},
+{
+    'name': 'David Eisenberg',
+    'Email': 'davidberg@gmail.com'
+},
+{
+    'name': 'Eva Fischer',
+    'Email': 'eva@gmail.com'
+},
+
+{
+    'name': 'Emmanuel Mauer',
+    'Email': 'emmanuelMa@gmail.com'
+},
+
+{
+    'name': 'Marcel Bauer',
+    'Email': 'bauer@gmail.com'
+},
+
+{
+    'name': 'Tatjana Wolf',
+    'Email': 'wolf@gmail.com'
+},];
